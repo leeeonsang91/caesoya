5675a7eaf1f463c39e384482d78e87eb2888d85c
eonsang <2eonhworking@gmail.com>
2020-08-03 15:17:12 +0900

    default

---
+++ dev/assets/js/function.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  $('.ptable table').basictable({
+    breakpoint: 1023,
+  });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  theme: {
+    theme: {
+      spacing: {
+        sm: "8px",
+        md: "12px",
+        lg: "16px",
+        xl: "24px"
+      }
+    },
+    screens: {
+      lg: { max: "1200px" },
+      md: { max: "768px" },
+      sm: { max: "450px" }
+    },
+    extend: {
+      colors: {
+        "theme-red": "#ea475c",
+        "theme-green": "#0b9a63",
+        "theme-lightblue": "#68c4c6",
+      }
+    },
+  },
+  plugins: [require("@tailwindcss/custom-forms")]
+};
